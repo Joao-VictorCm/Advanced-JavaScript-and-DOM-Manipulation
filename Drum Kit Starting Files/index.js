ba076926a6c63c0ba4f82d4f8e68e65f2bf27fac
Joao-VictorCm <joaovictor27cm@gmail.com>
2024-07-17 22:02:55 -0300

    Iniciando com funções de click

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,16 @@
+// Link de apoio com mais funções de botão
+// https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+// https://developer.mozilla.org/en-US/docs/Web/Events
+
+
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++){
+
+    document.querySelector(".drum")[i].addEventListener("click", function (){
+        alert("clicado")
+    });
+
+}
+
+
