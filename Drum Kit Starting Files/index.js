6741ac9034edc6c1d7d5d121b88bcb1cbbdbbb2f
Joao-VictorCm <joaovictor27cm@gmail.com>
2024-07-20 20:01:04 -0300

    Praticando this e switch

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -2,21 +2,55 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
 // https://developer.mozilla.org/en-US/docs/Web/Events
 
-
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
+for (var i = 0; i < numberOfDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var buttonInnerHTML = this.innerHTML;
 
-for (var i = 0; i < numberOfDrumButtons; i++){
+    console.log(buttonInnerHTML);
 
-    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+    switch (buttonInnerHTML) {
+      case "w":
+        audio = new Audio("./sounds/crash.mp3");
+        audio.play();
+        break;
+
+      case "a":
+        audio = new Audio("./sounds/kick-bass.mp3");
+        audio.play();
+        break;
+
+      case "s":
+        audio = new Audio("./sounds/tom-1.mp3");
+        audio.play();
+        break;
+
+      case "d":
+        audio = new Audio("./sounds/snare.mp3");
+        audio.play();
+        break;
+
+      case "j":
+        audio = new Audio("./sounds/tom-2.mp3");
+        audio.play();
+        break;
     
-       this.style.color = "blue"
-       
-    });
+
+      case "k":
+        audio = new Audio("./sounds/tom-3.mp3");
+        audio.play()
+        break;
 
 
+      case "l":
+        audio = new Audio("./sounds/tom-4.mp3");
+        audio.play()
+        break;
+
+
+      default:
+        break;
+    }
+  });
 }
-
-// var audioTest = new Audio('./sounds/tom-1.mp3')
-// audioTest.play()
-
