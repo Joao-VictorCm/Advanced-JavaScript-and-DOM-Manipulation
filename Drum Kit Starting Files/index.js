3361b8c40360a0920a0bbe7072d458189940f682
Joao-VictorCm <joaovictor27cm@gmail.com>
2024-07-20 19:42:13 -0300

    Conhecendo a variavel this

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -5,13 +5,18 @@
 
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
+
 for (var i = 0; i < numberOfDrumButtons; i++){
 
-    document.querySelector(".drum")[i].addEventListener("click", function (){
-        alert("clicado")
+    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+    
+       this.style.color = "blue"
+       
     });
 
 
 }
 
+// var audioTest = new Audio('./sounds/tom-1.mp3')
+// audioTest.play()
 
